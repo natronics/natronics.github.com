722fa72ba3124b614d2eddebbe10615b96a54e0c
Nathan Bergey <nathan.bergey@gmail.com>
2014-09-27 18:36:17 -0700

    GPS viz 1 blag

---
+++ notebooks/chart.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+red = '#e31d1d'
+green = '#76e146'
+blue = '#709afa'
+
+class Plot(object):
+
+    def __init__(self, title, xlabel="Time [s]", ylabel="", yrange=None, tend=5.0e-9):
+        self.ax = plt.figure(figsize=(16,7))
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        self.ax.axes[0].grid(color='grey', alpha=0.2, linestyle='dashed', linewidth=0.5)
+        if yrange is not None:
+            self.ax.axes[0].set_ylim(yrange)
+        else:
+            self.ax.axes[0].set_ylim([-1.5, 1.5])
+        self.ax.axes[0].set_xlim([0,tend])
+
+    def signal(self, f, color=red, alpha=0.8, lw=1.73, label="signal"):
+        x = []
+        y = []
+        for i in xrange(1600000):
+            t = i*1.0e-11
+            x.append(t)
+            y.append(f(t))
+        plt.plot(x, y, color, alpha=alpha, lw=lw, label=label)
+        return self
+
+    def show(self):
+        plt.show()
